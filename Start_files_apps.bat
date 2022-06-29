0c251268663b58e56a595d34d06b43ba724d3289
Daniel BRANCUS <81643384+dbrancusIcode4u@users.noreply.github.com>
2022-06-29 17:58:33 +0300

    Add files via upload

---
+++ Start_files_apps.bat
@@ -0,0 +1,29 @@
+@echo off
+ECHO ========================================================
+ECHO Marim memoria virtuala pentru containerul ElasticSearch!
+wsl -d docker-desktop sysctl -w vm.max_map_count=262144
+
+start C:\Users\DBR19488\AppData\Local\slack\slack.exe
+start excel "C:\Users\DBR19488\Documents\### 220617 - How2AppsDev ###.xlsx"
+start "" "C:\Program Files\JetBrains\PhpStorm 2022.1.1\bin\phpstorm64.exe"
+start "" "outlook.exe"
+start "" C:\Users\DBR19488\AppData\Local\Microsoft\Teams\Update.exe --processStart "Teams.exe" --process-start-args "--profile=AAD"
+
+@echo off
+start chrome https://total.local/fr/login
+
+@echo off
+start chrome https://gitlab.boost.open.global/izberg/click_and_buy/click-and-buy/-/merge_requests
+
+@echo off
+start chrome https://jira.open-groupe.com/secure/RapidBoard.jspa?rapidView=656&projectKey=TOTALTMA
+
+@echo off
+start chrome https://www.google.com/search?q=traducere+franceza&ei=LNqxYpb-FYnd7_UP8ce64Ao&ved=0ahUKEwiWxL365L74AhWJ7rsIHfGjDqwQ4dUDCA4&uact=5&oq=traducere+franceza&gs_lcp=Cgdnd3Mtd2l6EAMyCAgAEIAEELEDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIABBHELADOgcIABCwAxBDOhIILhDHARCjAhDIAxCwAxBDGAE6EgguEMcBENEDEMgDELADEEMYAToSCC4QxwEQrwEQyAMQsAMQQxgBOgsIABCABBCxAxCDAToICAAQgAQQyQNKBAhBGABKBAhGGABQ5gJYtRFgpRRoAXABeACAAVWIAa8FkgEBOZgBAKABAcgBDcABAdoBBAgBGAg&sclient=gws-wiz
+
+:: Aici e pus fisierul 
+:: explorer shell:startup
+
+
+C:\Users\DBR19488\AppData\Local\slack\slack.exe
+d
